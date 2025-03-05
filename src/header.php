95209206cb8fef38518a8c1d1b99785381c307b1
Joerg Mertin <joerg.mertin@broadcom.com>
2025-03-05 10:30:30 +0100

    Code cleanup.

--- src/header.php
+++ src/header.php
@@ -1,29 +1,31 @@
 <?php
-// This is a very handy script for troubleshooting.
-// Replace whdc.php with this script, and every request getting in will dump
-// full logs to the defined log file.
+/* This is a very handy script for troubleshooting.
+    Replace whdc.php with this script, and every request getting in will dump
+    full logs to the defined log file.
+*/
 
+// Timestamp
+$date =  date("Y-m-d H:i:s");
+
+// Get all data and assign it to variables.
 $headers = getallheaders();
 $resp = print_r($headers, true);
 $server = print_r($_SERVER, true);
 $post = print_r($_POST, true);
 $get = print_r($_GET, true);
+$json = file_get_contents('php://input');
 
 // Create handle for log file.
 $handle = fopen("/var/www/logs/whdc.log", "a");
 
-$date =  date("Y-m-d H:i:s");
-
-
+// Dump all to logfile.
 fwrite($handle, "\n*** $date ======================================================\n");
 fwrite($handle, "==> Headers ===================================================\n{$resp} \n");
 fwrite($handle, "==> GET =======================================================\n{$get} \n");
 fwrite($handle, "==> POST ======================================================\n{$post} \n");
-$json = file_get_contents('php://input');
 fwrite($handle, "==> JSON ======================================================\n{$json} \n");
 fwrite($handle, "==> SERVER ====================================================\n {$server} \n");
 // Close log-file.
 fclose($handle);
-
 
 ?>
